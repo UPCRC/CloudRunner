3605ce13af0eac068f76e7ad42d6c38c133a7057
AJAbanto <abantoalfred.work@gmail.com>
2024-05-10 00:54:07 +0800

    Added docs

--- examples/PID_line_tracer/PID_line_tracer.ino
+++ examples/PID_line_tracer/PID_line_tracer.ino
@@ -1,26 +1,22 @@
 
-#include "CloudRunner.h"
+#include <CloudRunner.h>
 CloudRunner board;
 void setup() {
   //Initialize board and motors
   board.begin();
-
-  //Beep motors twice
-  board.motor_quartersec_beep(2);
+  Serial.begin(9600);
+  Serial.println("Start Calibration");
   //Manually Calibrate for 10s
-  for(int i = 0; i<10; i++){
+  for(int i = 0; i<5; i++){
     board.calibrate_sensors();
-    board.calibrate_turn_sensors();
-    delay(1000);
+    delay(500);
+    Serial.println(i);
   }
-
-  //Beep motors thrice
-  board.motor_quartersec_beep(3);
-
-  //Wait 5s and beep every second
-  for(int i=0; i<5; i++){
-    board.beep_motor();
-    delay(1000);
+  Serial.println("Done Calibration");
+  
+  while(1){
+    Serial.println(board.getpos());
+    
   }
 }
 
